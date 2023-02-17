e320672d0ef3c90504ea9ddb9d371645608478c9
unknown <Hongofficial4444@gmail.com>
2023-02-17 17:59:47 +0700

    homepage

--- resources/website/views/pages/home.blade.php
+++ resources/website/views/pages/home.blade.php
@@ -17,21 +17,29 @@
                             <div class="grid-icon">
                                 <h1 class="text-xl text-gray-900 mt-4">What are you looking for?</h1>
                                 
-                                <ul class="flex">
-                                    <span class="box-icon rounded-md shadow-md">
-                                        <a href="">Home</a>
-                                    </span>
-                                    <span class="box-icon rounded-md shadow-md">
-                                        <a href="">Booking</a>
-                                    </span>
-                                    <span class="box-icon rounded-md shadow-md">
-                                        <a href="">Product</a>
-                                    </span>
+                                <ul class="flex items-center">
+                                    <div class="box-icon rounded-md shadow-md">
+                                        <a href="#">
+                                            <i class="h-10 w-10 ml-4 text-pink-600" data-feather="home"></i>
+                                            <h6 class="mt-3 text-base">Home</h6>
+                                        </a>
+                                    </div>
+                                    <div class="box-icon rounded-md shadow-md">
+                                        <a href="#">
+                                            <i class="h-10 w-10 ml-4 text-pink-600" data-feather="bookmark"></i>
+                                            <h6 class="mt-3 text-base">Booking</h6>
+                                        </a>
+                                    </div>
+                                    <div class="box-icon rounded-md shadow-md">
+                                        <a href="#">
+                                            <i class="h-10 w-10 ml-4 text-pink-600" data-feather="box"></i>
+                                            <h6 class="mt-3 text-base">Product</h6>
+                                        </a>
+                                    </div>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    
                 </div>
                 <div class="sale-section-wrapper-image">
                     <div class="">
@@ -46,18 +54,18 @@
                 <h2 class="text-3xl mt-5 font-semibold text-black">Latest For Sale</h2>
                 <hr>
             </div>
-            <div class="grid grid-cols-3 gap-8 mt-10">
+            <div class="grid grid-cols-3 gap-6 mt-10">
                 <div class="bg-white product-box shadow-lg">
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/22.jpg" alt="">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Little Acorn Farm</h1>
-                        <h2 class="text-lg font-semibold">$6558.00*</h2>
-                        <p>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$6558.00*</h2>
+                        <p class="text-base">
                             Real estate is divided into several categories, including residential property, commercial property and industrial property.
                         </p>
                         <div class="flex justify-between items-center">
@@ -70,12 +78,12 @@
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/24.jpg" alt="">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Home in Merrick Way</h1>
-                        <h2 class="text-lg font-semibold">$6558.00*</h2>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$6558.00*</h2>
                         <p>
                             Elegant retreat in a quiet Coral Gables setting. This home provides wonderful entertaining spaces with a chef kitchen opening…
                         </p>
@@ -89,13 +97,13 @@
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/25.jpg" alt="Property-3">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Little Acorn Farm</h1>
-                        <h2 class="text-lg font-semibold">$6558.00*</h2>
-                        <p>Real estate is divided into several categories, including residential property, commercial property and industrial property.</p>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$6558.00*</h2>
+                        <p class="text-base">Real estate is divided into several categories, including residential property, commercial property and industrial property.</p>
                         <div class="flex justify-between items-center">
                             <h3 class="text-gray-500 font-semibold text-sm">August 4, 2022</h3>
                             <button class="px-6 py-2 bg-white rounded-full border-solid border-2 border-gray-300">Detail</button>
@@ -103,18 +111,18 @@
                     </div>
                 </div>
             </div>
-            <div class="grid grid-cols-3 gap-8 mt-10">
+            <div class="grid grid-cols-3 gap-6 mt-10">
                 <div class="bg-white product-box shadow-lg">
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/22.jpg" alt="">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Little Acorn Farm</h1>
-                        <h2 class="text-lg font-semibold">$6558.00*</h2>
-                        <p>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$6558.00*</h2>
+                        <p class="text-base">
                             Real estate is divided into several categories, including residential property, commercial property and industrial property.
                         </p>
                         <div class="flex justify-between items-center">
@@ -127,13 +135,13 @@
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/24.jpg" alt="">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Home in Merrick Way</h1>
-                        <h2 class="text-lg font-semibold">$6558.00*</h2>
-                        <p>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$6558.00*</h2>
+                        <p class="text-base">
                             Elegant retreat in a quiet Coral Gables setting. This home provides wonderful entertaining spaces with a chef kitchen opening…
                         </p>
                         <div class="flex justify-between items-center">
@@ -146,13 +154,13 @@
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/25.jpg" alt="Property-3">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Little Acorn Farm</h1>
-                        <h2 class="text-lg font-semibold">$6558.00*</h2>
-                        <p>Real estate is divided into several categories, including residential property, commercial property and industrial property.</p>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$6558.00*</h2>
+                        <p class="text-base">Real estate is divided into several categories, including residential property, commercial property and industrial property.</p>
                         <div class="flex justify-between items-center">
                             <h3 class="text-gray-500 font-semibold text-sm">August 4, 2022</h3>
                             <button class="px-6 py-2 bg-white rounded-full border-solid border-2 border-gray-300">Detail</button>
@@ -177,11 +185,11 @@
             </div>
             <div class="slide-2 bg-white rounded-md p-8">
                 <div class="slide-2-icon">
-                    <a href="#"><i class="h-6 w-6 " data-feather="heart"></i></a>
+                    <a href="#" title="favorite"><i class="h-6 w-6 " data-feather="heart"></i></a>
                 </div>
                 <h1 class="text-xl font-semibold mt-4">Little Acorn Farm</h1>
-                <h2 class="text-lg font-semibold mt-2">$6558.00*</h2>
-                <p>Real estate is divided into several categories, including residential property, commercial property and industrial property. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero molestias ratione quos blanditiis error ducimus quae provident perspiciatis dolores cupiditate.</p>
+                <h2 class="text-lg font-semibold mt-2 mb-2">$6558.00*</h2>
+                <p class="text-base">Real estate is divided into several categories, including residential property, commercial property and industrial property. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero molestias ratione quos blanditiis error ducimus quae provident perspiciatis dolores cupiditate.</p>
                 <div class="flex justify-between items-center mt-3">
                     <h3 class="text-gray-500 font-semibold text-sm">August 4, 2022</h3>
                     <button class="px-6 py-2 bg-white rounded-full border-solid border-2 border-gray-300">Detail</button>
@@ -196,18 +204,18 @@
                 <h2 class="text-3xl mt-5 font-semibold text-black">Latest For Rent</h2>
                 <hr>
             </div>
-            <div class="grid grid-cols-3 gap-8 mt-10">
+            <div class="grid grid-cols-3 gap-6 mt-10">
                 <div class="bg-white product-box shadow-lg">
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/22.jpg" alt="">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Merrick in Spring Way</h1>
-                        <h2 class="text-lg font-semibold">$4513.00*</h2>
-                        <p>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$4513.00*</h2>
+                        <p class="text-base">
                             Elegant retreat in a quiet Coral Gables setting. This home provides wonderful entertaining spaces with a chef kitchen opening…
                         </p>
                         <div class="flex justify-between items-center">
@@ -220,13 +228,13 @@
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/24.jpg" alt="">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Allen's Across Way</h1>
-                        <h2 class="text-lg font-semibold">$6558.00*</h2>
-                        <p>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$6558.00*</h2>
+                        <p class="text-base">
                             This home provides wonderful entertaining spaces with a chef kitchen opening. Elegant retreat in a quiet Coral Gables setting..
                         </p>
                         <div class="flex justify-between items-center">
@@ -239,13 +247,13 @@
                     <div class="product-box-image">
                         <img src="https://themes.pixelstrap.com/sheltos/assets/images/property/25.jpg" alt="Property-3">
                         <div class="product-box-image-icon">
-                            <a href="#"><i class="h-5 w-5" data-feather="heart"></i></a>
+                            <a href="#" title="favorite"><i class="h-5 w-5" data-feather="heart"></i></a>
                         </div>
                     </div>
                     <div class="product-box-content p-8">
                         <h1 class="text-xl font-semibold ">Hidden Spring Hideway</h1>
-                        <h2 class="text-lg font-semibold">$9955.00*</h2>
-                        <p>
+                        <h2 class="text-lg font-semibold mt-2 mb-2">$9955.00*</h2>
+                        <p class="text-base">
                             Elegant retreat in a quiet Coral Gables setting. This home provides wonderful entertaining spaces with a chef kitchen opening…
                         </p>
                         <div class="flex justify-between items-center">
@@ -267,14 +275,17 @@
         <div class="buy-section-title">
             <span>Buy Or Sell</span>
         </div>
-        <div class="buy-section-slide container">
-            <h1 class="font-semibold text-lg text-white">SHELTOS REAL ESTATE</h1>
-            <h2 class="text-3xl text-white mt-10">Looking To Buy A New Property Or Sell An Existing One?<br> Real Homes Provides An Easy Solution!</h2>
-            <div class="banner-button w-[45%] mx-auto">
-                <a href="#" class="py-2 px-4 bg-primary rounded-full font-semibold text-white hover:text-white">Submit Property</a>
-                <a href="#" class="py-2 px-4 bg-white rounded-full font-semibold text-primary hover:text-primary">Browse Property</a>
+        <div class="container">
+            <div class="buy-section-slide ">
+                <h1 class="font-semibold text-lg text-white">SHELTOS REAL ESTATE</h1>
+                <h2 class="text-3xl text-white mt-10">Looking To Buy A New Property Or Sell An Existing One?<br> Real Homes Provides An Easy Solution!</h2>
+                <div class="banner-button w-[45%] mx-auto">
+                    <a href="#" class="py-2 px-4 bg-primary rounded-full font-semibold text-white hover:text-white">Submit Property</a>
+                    <a href="#" class="py-2 px-4 bg-white rounded-full font-semibold text-primary hover:text-primary">Browse Property</a>
+                </div>
             </div>
         </div>
+        
     </div>
 
     <!-- space -->
